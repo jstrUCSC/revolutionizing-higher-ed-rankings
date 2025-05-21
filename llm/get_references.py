1d80f54fcac550c7b2ca0594565ec4ee70872096
mainoahmuna <munam@oregonstate.edu>
2025-05-21 11:40:16 -0700

    final cvpr papers

--- llm/get_references.py
+++ llm/get_references.py
@@ -496,7 +496,7 @@ def save_to_csv(selected_refs: List[Tuple[int, str]], output_file="selected_refe
 
 def main():
     device = "cuda" if torch.cuda.is_available() else "cpu"
-    pdf_path = "../get_paper/Publications/CVPR_2021/Zhou_Patch2Pix_Epipolar-Guided_Pixel-Level_Correspondences_CVPR_2021_paper.pdf"
+    pdf_path = "../get_paper/Publications/CVPR_2020/Thermal_Image_Super-Resolution_Challenge_-_PBVS_2020.pdf"
     print(f"Reading PDF: {pdf_path}")
     content = read_pdf(pdf_path)
     if not content.strip():
